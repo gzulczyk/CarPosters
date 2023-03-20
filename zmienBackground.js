36101b1c3ac73f3ee1b6aa33f60f7a50dafe6bd9
Grzegorz <128420815+gzulczyk@users.noreply.github.com>
2023-03-20 20:35:54 +0100

    Delete zmienBackground.js

--- zmienBackground.js
+++
@@ -1,9 +0,0 @@
-	function zmienBackground()
-	{
-		var tlo = document.getElementById('faktyczneTlo');
-		document.getElementById("bfp"+cyfraObrazka).style = "border: 3px solid #fff";
-		cyfraObrazka++;
-		if(cyfraObrazka == 5){cyfraObrazka = 1; tlo.src="https://carposters.pl/wp-content/themes/bam/ikonki/bfp"+cyfraObrazka+".jpg"; document.getElementById("bfp"+cyfraObrazka).style =  "border: 3px solid #fff"; document.getElementById("bfp"+cyfraObrazka).style = "border: 3px solid #ffbc03";}
-		tlo.src="https://carposters.pl/wp-content/themes/bam/ikonki/bfp"+cyfraObrazka+".jpg";
-		document.getElementById("bfp"+cyfraObrazka).style =  "border: 3px solid #ffbc03";
-	}
